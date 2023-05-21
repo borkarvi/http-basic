8298ae73bc7be5534bed6f8a736158077c94fa99
Poonam2001@20 <borkarvi>
2023-05-21 20:43:21 +0530

    3 commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import AddUser from './components/AddUser/AddUser';
 
 function App() {
   let userData: any = [];
   const [users , setUsers] = useState([])
+
+  const getAllUserAxios = () => {
+    axios.get('http://localhost:3001/users').then(
+      (res) => {
+        const {allUsers } = res.data
+        console.log('allUsers from axios' , allUsers)
+        userData = allUsers;
+       setUsers(allUsers)
+ }
+     ).catch(
+      (err) => {
+        console.log('err from axios' , err.message)
+    }
+     )
+  
+  }
   
   useEffect(() => {
-
-    axios.get('http://localhost:3001/users').then(
-     (res) => {
-       const {allUsers } = res.data
-       console.log('allUsers from axios' , allUsers)
-       userData = allUsers;
-       setUsers(allUsers)
-}
-    ).catch(
-     (err) => {
-       console.log('err from axios' , err.message)
-   }
-    )
+        getAllUserAxios();
    } ,[])
+
    console.log('userData' , userData)
+   console.log('users' , users)
+
 
     const allUsersHTML = users.map((user: any,index) => {
       return(
@@ -34,6 +42,7 @@ function App() {
      <div>
       <p>HTTP</p>
       {allUsersHTML}
+      <AddUser/>
        </div>
   );
 }
